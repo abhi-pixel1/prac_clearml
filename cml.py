b9b77fa5d085573c4915bcc8426bd02577711506
abhi-pixel1 <bharadwaj.abhi.r@gmail.com>
2024-06-06 21:28:14 +0530

    cml.py update

--- cml.py
+++ cml.py
@@ -12,6 +12,8 @@ args = parser.parse_args()
 task = Task.init(project_name='clearml_prac1', task_name='apple_data_download1')
 
 # getting a local copy of the dataset
-dataset_folder = Dataset.get(dataset_name='apple_stock1', dataset_project='clearml_prac1').get_local_copy()
+# dataset_folder = Dataset.get(dataset_name='apple_stock1', dataset_project='clearml_prac1').get_local_copy()
+dataset_folder = Dataset.get(dataset_id=args.dataset).get_local_copy()
+
 
 # go over the files in `dataset_folder` and train your model
